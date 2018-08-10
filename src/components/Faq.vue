0c367f7d94dd58b308baa815ab0d58a403f39d83
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-08-10 23:44:49 +0200

    Updates in FAQ

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -5,12 +5,12 @@
         <h1 class="title">FAQ</h1>
         <h2 class="subtitle is-4">THIS IS OUR FAQ PAGE</h2>
 
-        <div class="columns">
-          <div class="column is-one-third">
+        <div class="columns" v-if="faqs && faqs.lenght">
+          <div class="column is-one-third" v-for="faq in faqs" :key="faq.title">
             <div class="card">
               <div class="card-content">
-                <p class="title">THE TITLE</p>
-                <p class="answer">The Answer</p>
+                <p class="title">{{ faq.title }}</p>
+                <p class="answer">{{ faq.body }}</p>
               </div>
             </div>
           </div>
@@ -22,8 +22,24 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'faq'
+  name: 'faq',
+  data: () => ({
+    faqs: [],
+    errors: []
+  }),
+
+  created() {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+      .ten(response => {
+        this.faqs = response.data.slice(0, 10);
+      })
+      .catch(e =>  {
+        this.errors.push(e)
+      })
+  }
 }
 </script>
 
